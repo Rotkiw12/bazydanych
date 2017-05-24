9b93d96ad1f65d0a28796978f997d1bc481ab531
Wiktor Saj <wsaj@zssalezjanie.com>
2017-05-24 11:31:50 +0200

    nowe

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-
-
-
+$polaczenie=mysql_connect('localhost','root','') or die('brak polaczenia z baza danych');
+$tmp=mysql_select_db('uczniowie',$polaczenie) or die('nie moge wybrac okreslonej bazy danych');
+print_r($polaczenie);
+print_r($tmp);
+$add=mysql_query("insert into uczniowie values(null,'Zygmunt','Nowak',21)");
+print_r($add);
 ?>
