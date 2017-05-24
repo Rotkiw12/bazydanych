4bbf5e1e9578576ce51e5983fbf50ad3dd85b825
Wiktor Saj <wsaj@zssalezjanie.com>
2017-05-24 12:05:43 +0200

    Połączenie z bazą danych

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
-$polaczenie=mysql_connect('localhost','root','') or die('brak polaczenia z baza danych');
-$tmp=mysql_select_db('uczniowie',$polaczenie) or die('nie moge wybrac okreslonej bazy danych');
+$dbhost='localhost';
+$dbuser='root';
+$dbpass='';
+$dbname='articles';
+$polaczenie=mysql_connect($dbhost,$dbuser,$dbpass) or die('brak polaczenia z baza danych');
+$tmp=mysql_select_db($dbname,$polaczenie) or die('nie moge wybrac okreslonej bazy danych');
 print_r($polaczenie);
 print_r($tmp);
 $add=mysql_query("insert into uczniowie values(null,'Zygmunt','Nowak',21)");
